07629af443fe6cc41350f85e97d86a0f76a819bd
刘浩天 <2410651402@qq.com>
2023-03-27 21:38:59 +0800

    first

---
+++ PrimalEditor/Utilities/Utilities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimalEditor.Utilities
+{
+    public static class ID
+    {
+        public static int INVALID_ID => -1;
+        public static bool IsValid(int id) => id != INVALID_ID;
+    }
+
+
+
+    public static class MathUtil
+    {
+        public static float Epsion => 0.00001f;
+
+        public static bool IsTheSameAs(this float value,float other)
+        {
+            return Math.Abs(value - other) < Epsion;
+        }
+
+        public static bool IsTheSameAs(this float? value, float? other)
+        {
+            if(!value.HasValue || !other.HasValue) return false;
+            return Math.Abs(value.Value - other.Value) < Epsion;
+        }
+    }
+}
